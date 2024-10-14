5a2a5464a138469a24edd77acf8269d3677e417b
Filippo Venturi <fventuri@onit.it>
2024-10-14 12:21:08 +0200

    Aggiunto esercizio 4 su routes varie

---
+++ Laboratorio2/Laboratorio2.Web/Areas/Configura/Utenti/UtentiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Laboratorio2.Web.Areas.Configura.Utenti
+{
+    // ES4: Raggiungere queste actions
+    [Area("Configura")]
+    public partial class UtentiController : Controller
+    {
+        [HttpGet]
+        public async virtual System.Threading.Tasks.Task<IActionResult> Index()
+        {
+            var test = "pluto";
+
+            return View();
+        }
+
+        [HttpGet]
+        public async virtual System.Threading.Tasks.Task<IActionResult> Edit(Guid id)
+        {
+            var test = "pluto";
+
+            return View(id);
+        }
+    }
+}
